e74511ada4b91061e25f08a31d40d1f849aefbc5
Tynan Ford <tford@lbl.gov>
2021-06-12 01:07:19 -0700

    update buttons to icons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,15 @@
-import { Button, AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, Tooltip} from '@material-ui/core';
+import { PlayCircleFilledWhite, Stop, SkipNext, SkipPrevious, FastForward, FastRewind, Clear } from '@material-ui/icons';
 
 function Header(props) {
+    const largePadding = {
+        paddingRight: 30,
+        paddingLeft: 30,
+    }
+    const speedPadding = {
+        width: 120,
+    }
+
     return(
         <AppBar position="fixed">
             <Toolbar>
@@ -8,14 +17,46 @@ function Header(props) {
                     <Typography variant="h2">{props.song}</Typography>
                 </div>
                 <div style={{display:"flex"}}>
-                    <Button color="secondary" onClick={() => {props.play(true)}}>Start</Button>
-                    <Button color="secondary" onClick={() => {props.play(false)}}>Stop</Button>
-                    <Button color="secondary" onClick={() => {props.switchSong(-1)}}>Back</Button>
-                    <Button color="secondary" onClick={() => {props.switchSong(1)}}>Forward</Button>
-                    <Button color="secondary" onClick={() => {props.changeSpeed(props.speedAdjustment + 500)}}>Slow Down</Button>
-                    <Typography variant="h6">{-1*props.speedAdjustment}</Typography>
-                    <Button color="secondary" onClick={() => {props.changeSpeed(props.speedAdjustment - 500)}}>Speed Up</Button>
-                    <Button color="secondary" onClick={() => {props.changeSpeed(0)}}>Reset</Button>
+                    <Tooltip title={<h1>Start</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.play(true)}}>
+                            <PlayCircleFilledWhite fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Stop</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.play(false)}}>
+                            <Stop fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Back</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.switchSong(-1)}}>
+                            <SkipPrevious fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Forward</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.switchSong(1)}}>
+                            <SkipNext fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Slow 500ms</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.changeSpeed(props.speedAdjustment + 500)}}>
+                            <FastRewind fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Speed Adjustment</h1>} arrow>
+                        <Typography style={speedPadding} color="secondary" variant="h3" align="center">
+                            {-1*props.speedAdjustment}
+                        </Typography>
+                    </Tooltip>
+                    <Tooltip title={<h1>Speed 500ms</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.changeSpeed(props.speedAdjustment - 500)}}>
+                            <FastForward fontSize="large" />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title={<h1>Reset Adjustment</h1>} arrow>
+                        <IconButton style={largePadding} color="inherit" onClick={() => {props.changeSpeed(0)}}>
+                            <Clear fontSize="large"/>
+                        </IconButton>
+                    </Tooltip>
                 </div>
             </Toolbar>
         </AppBar>
